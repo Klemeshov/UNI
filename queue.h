7b6251bf90306f001c4b77324784789e1ee0bf94
Klemeshov <dima-0510@mail.ru>
2021-04-20 23:42:07 +0300

    delete useless files

--- queue.h
+++ queue.h
@@ -1,14 +1,31 @@
-//
-// Created by dima- on 20.04.2021.
-//
-
 #ifndef GRAPH_PROJECT_QUEUE_H
 #define GRAPH_PROJECT_QUEUE_H
 
 
-class queue {
-
+template<typename V>
+class queue{
+public:
+    queue(){
+        f = 0;
+    }
+    void push (V e){
+        this->array.push_back(e);
+    }
+    V pop(){
+        if (this->f == this->array.size())
+            return nullptr;
+        else
+            return this->array[this->f++];
+    }
+    V top(){
+        if (this->f == this->array.size())
+            return nullptr;
+        else
+            return this->array[this->f];
+    }
+private:
+    std::vector<V> array;
+    int f;
 };
 
-
-#endif //GRAPH_PROJECT_QUEUE_H
+#endif
